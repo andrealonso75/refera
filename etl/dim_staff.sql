8985355d044c1b38503bd19436f777d98538dc92
Andre <65608767+andrealonso75@users.noreply.github.com>
2023-05-10 17:26:09 -0300

    Add files via upload

---
+++ etl/dim_staff.sql
@@ -0,0 +1,33 @@
+-- DIM STAFF
+
+-- DROP TABLE IF EXISTS staff_dim;
+-- CREATE TABLE staff_dim (
+-- 				staff_id INTEGER PRIMARY KEY,
+-- 				first_name TEXT,
+-- 				last_name TEXT,
+-- 				address_id INTEGER,
+-- 				email TEXT,
+-- 				store_id INTEGER
+-- 	);
+	
+-- 	INSERT INTO staff_dim(
+-- 					 	    staff_id,
+-- 							first_name,
+-- 							last_name,
+-- 							address_id,
+-- 							email,
+-- 							store_id
+-- 					)
+
+-- SELECT 
+-- 		staff_id,
+-- 		first_name,
+-- 		last_name,
+-- 		address_id,
+-- 		email,
+-- 		store_id
+		
+-- FROM staff
+
+select * from staff_dim
+
